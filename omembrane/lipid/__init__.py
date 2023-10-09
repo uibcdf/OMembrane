fe1e9dbf1450c7519efb87b031c43b58dd2aecaf
Diego Prada-Gracia <prada.gracia@gmail.com>
2023-10-08 22:17:44 -0600

    in process

--- omembrane/lipid/__init__.py
+++ omembrane/lipid/__init__.py
@@ -1,3 +1,6 @@
+from .name import name
+from .family import family
+
 from .get_surface_area import get_surface_area
 from .get_radius import get_radius
 from .get_random_lipid import get_random_lipid
