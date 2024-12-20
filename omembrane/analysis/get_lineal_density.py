6bd58e664f457f97a0eb61a4b65943c0a80b14dd
Diego Prada <prada.gracia@gmail.com>
2024-12-20 16:40:50 -0600

    in process

--- omembrane/analysis/get_lineal_density.py
+++ omembrane/analysis/get_lineal_density.py
@@ -2,73 +2,62 @@ import molsysmt as msm
 from molsysmt import pyunitwizard as puw
 import numpy as np
 
-def get_lineal_density(molecular_system, selection, axis=[0,0,1], bins=100, bin_length=None, range=None,
-        normalized=False, frequency=False, weight=None, syntax='MolSysMT'):
-
-    atom_indices = msm.select(molecular_system, selection=selection, syntax=syntax)
-
-    n_structures = msm.get(molecular_system, n_structures=True)
+def get_lineal_density(molecular_system, selection, structure_index=0,
+        axis=[0,0,1], bins=100, normalized=False, frequency=False,
+        weight=None, syntax='MolSysMT'):
 
     length_unit = puw.get_standard_units(dimensionality={'[L]':1})
 
-    if range is None:
-
-        iterator = msm.Iterator(molecular_system, selection = atom_indices, coordinates = True)
-
-        min_total = puw.quantity(np.inf,length_unit)
-        max_total = puw.quantity(-np.inf,length_unit)
-
-        for aux_coordinates in iterator:
-            proj = np.dot(aux_coordinates, axis)
-            min_coordinate = proj.min()
-            max_coordinate = proj.max()
-
-            if min_coordinate < min_total:
-                min_total = min_coordinate
-
-            if max_coordinate > max_total:
-                max_total = max_coordinate
-
-        range = [min_total, max_total]
-
-    else:
-
-        range=puw.quantity(range)
+    atom_indices = msm.select(molecular_system, selection=selection, syntax=syntax)
+    coordinates = msm.get(molecular_system, selection=atom_indices, structure_indices=structure_index, coordinates=True)
+    coordinates = puw.convert(coordinates, to_unit=length_unit)
+    box = puw.convert(box, to_unit=length_unit)
 
 
-    if bin_length is None:
-        bin_length = (range[1]-range[0])/bins
-    else:
-        bin_length=puw.quantity(bin_length)
-        bins = (range[1]-range[0])/bin_length
-        bins = int(np.ceil(bins))
-        range[1] = range[0]+bins*bin_length
+    # Hay que incluir aquí el wrap
+
+    box = puw.get_value(box)
+    coordinates = puw.get_value(coordinates)
+
+    min_q = np.inf
+    max_q = -np.inf
+
+    for ii in [0,1]:
+        for jj in [0,1]:
+            for kk in [0,1]:
+
+                vertex = ii*box[structure_index][0]+jj*box[structure_index][1]+kk*box[structure_index][2]
+                proj = np.dot(vertex, axis)
+
+                if proj<min_q:
+                    min_q = proj
+                elif proj>max_q:
+                    max_q = proj
+
+    bin_length = (max_q-min_q)/bins
 
     freq = np.zeros(bins)
 
-    iterator = msm.Iterator(molecular_system, selection = atom_indices, coordinates = True)
+    coordinates = msm.get(molecular_system, selection=atom_indices, structure_indices=structure_index, coordinates=True)
+    coordinates = puw.get_value(coordinates, to_unit=length_unit)
 
-    bin_length_value = puw.get_value(bin_length)
-    range_0_value = puw.get_value(range[0])
-
-    for aux_coordinates in iterator:
-        aux_coordinates = puw.get_value(aux_coordinates)
+    for aux_coordinates in coordinates[0,:]:
         proj = np.dot(aux_coordinates, axis)
-        proj = proj.flatten()
-
-        for ii in proj:
-            bin_index = int(np.floor((ii - range_0_value)/bin_length_value))
-            if bin_index==bins:
-                bin_index=bins-1
-            freq[bin_index]+=1
+        bin_index = int(np.floor((proj - min_q)/bin_length))
+        if bin_index==bins:
+            bin_index=bins-1
+        if bin_index>bins:
+            print(aux_coordinates)
+            print(box)
+        freq[bin_index]+=1
 
     if frequency:
         density = freq
     elif not normalized:
-        density = freq/(n_structures*bin_length)
+        density = freq/bin_length
     else:
-        density = freq/(n_structures*bin_length)
+        density = freq/bin_length
         density = density/density.sum()
 
-    return np.linspace(range[0], range[1], bins+1), density
+    return np.linspace(min_q, max_q, bins+1), density
 
---
+++ protocols.md
@@ -0,0 +1,7 @@
+# Where we can find protocols
+
+## VMD script
+http://www.ks.uiuc.edu/Research/vmd/plugins/membrane/
+
+https://github.com/callumjd/AMBER-Membrane_protein_tutorial
+
