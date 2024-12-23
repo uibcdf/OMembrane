60bdcde53894c08e3d8ef4f17130b7d639ac407b
Diego Prada <prada.gracia@gmail.com>
2024-12-23 15:44:32 -0600

    in process

--- omembrane/_version.py
+++ omembrane/_version.py
@@ -1,1 +1,1 @@
-__version__ = "0.1.0+36.g46c8aa8.dirty"
+__version__ = "0.1.0+38.g6bd58e6.dirty"
--- omembrane/analysis/get_lineal_density.py
+++ omembrane/analysis/get_lineal_density.py
@@ -4,17 +4,18 @@ import numpy as np
 
 def get_lineal_density(molecular_system, selection, structure_index=0,
         axis=[0,0,1], bins=100, normalized=False, frequency=False,
-        weight=None, syntax='MolSysMT'):
+        weight=None, origin_at_center=False, syntax='MolSysMT'):
 
     length_unit = puw.get_standard_units(dimensionality={'[L]':1})
 
     atom_indices = msm.select(molecular_system, selection=selection, syntax=syntax)
     coordinates = msm.get(molecular_system, selection=atom_indices, structure_indices=structure_index, coordinates=True)
+    box = msm.get(molecular_system, structure_indices=structure_index, box=True)
     coordinates = puw.convert(coordinates, to_unit=length_unit)
     box = puw.convert(box, to_unit=length_unit)
 
-
-    # Hay que incluir aquí el wrap
+    wrapped_dict = msm.pbc.wrap_to_pbc({'coordinates':coordinates, 'box':box}, origin_at_center=False)
+    coordinates = wrapped_dict['coordinates']
 
     box = puw.get_value(box)
     coordinates = puw.get_value(coordinates)
@@ -38,17 +39,9 @@ def get_lineal_density(molecular_system, selection, structure_index=0,
 
     freq = np.zeros(bins)
 
-    coordinates = msm.get(molecular_system, selection=atom_indices, structure_indices=structure_index, coordinates=True)
-    coordinates = puw.get_value(coordinates, to_unit=length_unit)
-
     for aux_coordinates in coordinates[0,:]:
         proj = np.dot(aux_coordinates, axis)
         bin_index = int(np.floor((proj - min_q)/bin_length))
-        if bin_index==bins:
-            bin_index=bins-1
-        if bin_index>bins:
-            print(aux_coordinates)
-            print(box)
         freq[bin_index]+=1
 
     if frequency:
